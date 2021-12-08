baa86a824e0c5fea7784e25cbb98e203fa1c9332
jmason87 <joewebdev87@gmail.com>
2021-12-08 15:44:01 -0600

    order page displays all previous orders made by that user. includes items prucased, date purchased, and total price

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,5 +1,42 @@
+import React, { useEffect, useState } from "react"
+
+
+
 export const Orders = () => {
+    const [orders, setOrders] = useState([])
+    const user = localStorage.getItem("pub_user")
+
+    
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/orders?_expand=appetizer&_expand=entree&_expand=dessert&_expand=drink&userId=${user}`)
+            .then(res => res.json())
+            .then((data) => {
+                setOrders(data)
+            })
+        },
+        []
+    )
+    
+    
     return (
+        <>
         <h1>Orders</h1>
+        {
+            orders.map((order) => {
+                
+                 return <div key={`order--${order.id}`}>
+                     <p>Order #{order.id} placed on {order.dateOrdered}:</p>
+                     <ul>
+                         <li>{order.appetizer.name}</li>
+                         <li>{order.entree.name}</li>
+                         <li>{order.dessert.name}</li>
+                         <li>{order.drink.name}</li>
+                     </ul>
+                     <p>Total price: ${order.appetizer.price + order.entree.price + order.dessert.price + order.drink.price}</p>
+                     </div>
+            })    
+        }
+        </>
     )
 }
