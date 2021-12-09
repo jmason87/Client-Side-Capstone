c959d98edeb04be63fe4d4f1b511f115775c9eae
jmason87 <joewebdev87@gmail.com>
2021-12-09 13:02:51 -0600

    Review Form post to api and adds new review to same page

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,5 +1,113 @@
+import { render } from "@testing-library/react"
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router"
+
 export const Reviews = () => {
+    const [locations, setLocations] = useState([])
+    const [reviewsForList, setReviewsForList] = useState([])
+    const [addReview, updateReviews] = useState([])
+    const [review, setReview] = useState({
+        locationId: 0,
+        post: ""
+    })
+    const history = useHistory()
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/locations")
+                .then(res => res.json())
+                .then((data) => {
+                    setLocations(data)
+                })
+        },
+        []
+    )
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/reviews")
+                .then(res => res.json())
+                .then((data) => {
+                    setReviewsForList(data)
+                })
+        },
+        [addReview]
+    )
+
+
+    const submitNewReview = () => {
+        //e.preventDefault()
+        const newReview = {
+            locationId: parseInt(review.locationId),
+            userId: parseInt(localStorage.getItem("pub_user")),
+            post: review.post
+        }
+
+        const fetchOption = {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json"
+            },
+            body: JSON.stringify(newReview)
+        }
+
+        return fetch("http://localhost:8088/reviews", fetchOption)
+            .then(res => res.json())
+            .then((data) => {
+                updateReviews(data)
+            })
+            .then(() => {
+                document.getElementById("reviewForm").reset()
+            })
+    }
+
     return (
-        <h1>Reviews</h1>
+        <>
+            <h1>Reviews</h1>
+            <form id="reviewForm">
+                <select
+                    onChange={
+                        (evt) => {
+                            const copy = { ...review }
+                            copy.locationId = evt.target.value
+                            setReview(copy)
+                        }
+                    }>
+                    <option value="" >Select a Location</option>
+                    {
+                        locations.map(location => {
+                            return <option key={location.id} value={location.id}>{location.neighborhood}</option>
+                        })
+                    }
+                </select>
+
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Leave a Review"
+                        onChange={
+                            (evt) => {
+                                const copy = { ...review }
+                                copy.post = evt.target.value
+                                setReview(copy)
+                            }
+                        } />
+                </div>
+            </form>
+
+            <button onClick={submitNewReview}>Submit</button>
+
+
+            {
+                reviewsForList.map(
+                    (reviewListObj) => {
+                        return <div>
+                            {reviewListObj.post}
+                        </div>
+                    }
+                )
+            }
+
+        </>
     )
 }
