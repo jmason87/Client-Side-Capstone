5b379067f53f9ce9c493dee9092ea22aef5dda7d
jmason87 <joewebdev87@gmail.com>
2021-12-16 13:26:19 -0600

    all the styles

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -54,19 +54,19 @@ export const Register = (props) => {
             </dialog>
 
             <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for Honey Rae Repairs</h1>
+                <h1 className="h3 mb-3 font-weight-normal">Nashville Software Speakeasy Registration</h1>
                 <fieldset>
-                    <label htmlFor="name"> Full Name </label>
+                    <label htmlFor="name"> {""} </label>
                     <input onChange={updateUser}
                            type="text" id="name" className="form-control"
                            placeholder="Enter your name" required autoFocus />
                 </fieldset>
                 <fieldset>
-                    <label htmlFor="email"> Email address </label>
+                    <label htmlFor="email"> {""} </label>
                     <input onChange={updateUser} type="email" id="email" className="form-control" placeholder="Email address" required />
                 </fieldset>
                 <fieldset>
-                    <button type="submit"> Register </button>
+                    <button type="submit" className="register--button"> Register </button>
                 </fieldset>
             </form>
         </main>
--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import "./Orders.css"
 
 
 
@@ -6,37 +7,39 @@ export const Orders = () => {
     const [orders, setOrders] = useState([])
     const user = localStorage.getItem("pub_user")
 
-    
+
     useEffect(
         () => {
             fetch(`http://localhost:8088/orders?_expand=appetizer&_expand=entree&_expand=dessert&_expand=drink&_expand=location&userId=${user}`)
-            .then(res => res.json())
-            .then((data) => {
-                setOrders(data)
-            })
+                .then(res => res.json())
+                .then((data) => {
+                    setOrders(data)
+                })
         },
         [user]
     )
-    
-    
+
+
     return (
         <>
-        <h1>Orders</h1>
-        {
-            orders.map((order) => {
-                
-                 return <div key={`order--${order.id}`}>
-                     <p>Order #{order.id} placed at the {order.location.neighborhood} location on {order.dateOrdered}:</p>
-                     <ul>
-                         <li>{order.appetizer.name}</li>
-                         <li>{order.entree.name}</li>
-                         <li>{order.dessert.name}</li>
-                         <li>{order.drink.name}</li>
-                     </ul>
-                     <p>Total price: ${order.appetizer.price + order.entree.price + order.dessert.price + order.drink.price}</p>
-                     </div>
-            })    
-        }
+            <h1>Orders</h1>
+            <div className="orders">
+                {
+                    orders.map((order) => {
+
+                        return <div key={`order--${order.id}`} className="orderCards">
+                            <p>Order #{order.id} placed at the {order.location.neighborhood} location on {order.dateOrdered}:</p>
+                            <ul>
+                                <li>{order.appetizer.name}</li>
+                                <li>{order.entree.name}</li>
+                                <li>{order.dessert.name}</li>
+                                <li>{order.drink.name}</li>
+                            </ul>
+                            <p>Total price: ${order.appetizer.price + order.entree.price + order.dessert.price + order.drink.price}</p>
+                        </div>
+                    })
+                }
+            </div>
         </>
     )
 }
